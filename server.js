bca9ffaad790169ab17101a5e1a56d9965b6f355
Blaine Jester <blainesworld@gmail.com>
2015-05-30 13:57:57 -0700

    Some refactoring of promise handling, addition of some doc blocks to methods, and start job structure

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var config = require('config');
 
 var BundleControl = require('lib/controller/bundle.js');
+var FactoringControl = require('lib/controller/factoring.js');
 var ResourceControl = require('lib/controller/resource.js');
 var BundleRoute = require('lib/route/bundle.js');
 var ResourceRoute = require('lib/route/resource.js');
@@ -21,7 +22,16 @@ var resRoute = new ResourceRoute('GET', '/resources/get');
 bunRoute.setAuth(auth);
 resRoute.setExpires(60*60*24*14);
 
-var bundleController = new BundleControl();
+if (config.get('factoring.enabled'))
+{
+  var bundleController = new FactoringControl();
+  bundleController.setLevels(config.get('factoring.levels'));
+}
+else
+{
+  var bundleController = new BundleControl();
+}
+
 var resourceController = new ResourceControl();
 bundleController.addRoute(bunRoute);
 resourceController.addRoute(resRoute);
