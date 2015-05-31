5753d515b159de1abcca25d5cb19c9a84325cc85
Blaine Jester <blainesworld@gmail.com>
2015-05-31 10:28:19 -0700

    Add istanbul for code coverage and update test to use expect

--- test/unit/server.js
+++ test/unit/server.js
@@ -1,4 +1,5 @@
 require('test/lib/bootstrap.js');
+var defer = require('deferred');
 var Server = require('lib/server.js');
 
 describe('Server', function()
@@ -10,12 +11,33 @@ describe('Server', function()
     it('should set config values for the connection', function(done)
     {
       var s = new Server(config);
-      assert.equal(1, s.connections.length);
+      expect(s.connections).to.have.length(1);
       
       var connection = s.connections[0];
-      assert.equal(config.get('server.host'), connection['info']['host']);
-      assert.equal(config.get('server.port'), connection['info']['port']);
+      expect(connection['info']['host']).to.equal(config.get('server.host'));
+      expect(connection['info']['port']).to.equal(config.get('server.port'));
       
+      done();
+    });
+    
+    it('should initialize the main deferred', function(done)
+    {
+      var s = new Server(config);
+      expect(s._defer).to.be.instanceOf(defer.Deferred);
+      done();
+    });
+    
+    it('should initialize the job queue', function(done)
+    {
+      var s = new Server(config);
+      expect(s._jobQueue).to.be.instanceOf(Array);
+      done();
+    });
+    
+    it('should initialize the controllers array', function(done)
+    {
+      var s = new Server(config);
+      expect(s._controllers).to.be.instanceOf(Array);
       done();
     });
   });
